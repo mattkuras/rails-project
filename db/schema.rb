59964dace40bc1ddc7d778ae423700cbebebb0e8
matt_kuras <mattkuras@gmail.com>
2020-05-28 17:58:42 -0400

    create community model with associations and migrate communitites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_215451) do
+ActiveRecord::Schema.define(version: 2020_05_28_215705) do
+
+  create_table "communities", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
