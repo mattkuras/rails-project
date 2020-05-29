46f0cb13ac9cb31686d442a712d2b792d4960aac
matt_kuras <mattkuras@gmail.com>
2020-05-29 13:32:23 -0400

    add show action to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,14 +13,15 @@ class PostsController < ApplicationController
         @post = current_user.posts.build(post_params)
 
       if @post.save
-        
         redirect_to posts_path
       else
-        
         render :new
       end
     end
 
+    def show 
+      @post = Post.all.find_by(id: params[:id]
+    end
 
     private 
     
