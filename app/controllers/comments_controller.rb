cda035ba726f8da0c357cce552f806881d750c38
matt_kuras <mattkuras@gmail.com>
2020-05-29 15:34:32 -0400

    add create action to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,14 @@ class CommentsController < ApplicationController
         @comment = Comment.new 
     end
 
+    def create 
+        comment = current_user.comments.build(content: params[:content])
+        comment.save 
+        if comment.save 
+            redirect_to(comment_path)
+        else 
+            redirect_to(comment_path)
+        end
+    end
+
 end
