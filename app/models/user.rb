9fbad926a4db1fd2f5ca4e05d2c7959a282f4146
matt_kuras <mattkuras@gmail.com>
2020-05-29 13:22:52 -0400

    add name method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
     has_many :posts 
     has_many :comments
     has_many :identities    
-    belongs_to :community
+    belongs_to :community, optional: true
 
     has_many :followers, class_name: "User", foreign_key: "followed_id"
 
-    belongs_to :followed, class_name: "User"
+    belongs_to :followed, class_name: "User", optional: true
+
+    def name 
+        self.first_name + " " + self.last_name
+    end
     
 end
