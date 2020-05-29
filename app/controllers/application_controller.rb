cecd21709e0eb79e739bb79b77da2ae0fde10247
matt_kuras <mattkuras@gmail.com>
2020-05-29 13:20:50 -0400

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    def verified_user
+        redirect_to '/' unless user_is_authenticated
+      end
+   
+     def user_is_authenticated
+       !!current_user
+     end
+   
+     def current_user
+       User.find_by(id: session[:user_id])
+     end
+     
 end
