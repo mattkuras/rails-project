5654f8f7b3fdcb6a8c5c6f970bb6e6132f6cd07b
matt_kuras <mattkuras@gmail.com>
2020-05-29 14:18:12 -0400

    migrate identites db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_220140) do
+ActiveRecord::Schema.define(version: 2020_05_29_181317) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_05_28_220140) do
   end
 
   create_table "communities", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "identities", force: :cascade do |t|
+    t.string "name"
+    t.string "progress"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
