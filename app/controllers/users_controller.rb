9789aaee47a665e55ada30e4767c878c67cdbc79
matt_kuras <mattkuras@gmail.com>
2020-05-29 13:21:35 -0400

    fix create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,17 @@ class UsersController < ApplicationController
 
     def create
 
-        user = User.new(user_params)
-        #   if user.save
-        #     session[:user_id] = user.id
+        user = User.create(user_params)
+        user.save
+
+        
+           if user.save
+             session[:user_id] = user.id
              redirect_to posts_path
-        #   else
-        #     render 'new'
-        #   end
+           else
+            byebug
+             render 'new'
+           end
         end
   
 
