9fc7a5b3f8122b8021b0003484f54d45a10eedd3
matt_kuras <mattkuras@gmail.com>
2020-06-01 14:04:27 -0400

    add method for strong params in identity

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,17 +1,30 @@
 class IdentitiesController < ApplicationController
     def new
         @new_identities = []
+        9.times do |t|
+        @new_identities << Identity.new
+        end
         @identities = ['Musician', "Writer", 'Father', 'Mother', 'Athlete', 'Traveler', 'Teacher', 'Artist', 'Cook']
     end
 
     def create
     byebug
         identity = current_user.identity.build(identity_params)
-        identity.save 
         if identity.save 
-            redirect_to posts_path 
+            redirect_to user_path(current_user)
         else
             render 'new'
         end 
     end
+
+    private 
+
+    def identity_params 
+        params.require(:identity).permit(
+            :name,
+            :standard,
+            :progress
+        )
+    end
+
 end
