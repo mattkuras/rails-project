d806d0bc0c24d30b4e454aab638f29ff238662d0
matt_kuras <mattkuras@gmail.com>
2020-06-03 17:04:00 -0400

    add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-    # validates :first_name, presence: true, length: { minimum: 2 }
-    # validates :first_name, presence: true, length: { minimum: 2 }
-    # validates :password, length: { in: 6..20 }
-    # validates :email, presence: true, uniqueness: true#, message: "that email has already been used"
+ 
 
     has_secure_password
     has_many :posts 
@@ -19,6 +16,11 @@ class User < ApplicationRecord
     has_many :followers, class_name: "User", foreign_key: "followed_id"
 
     belongs_to :followed, class_name: "User", optional: true
+
+    validates :first_name, presence: true, length: { minimum: 2 }
+    validates :last_name, presence: true, length: { minimum: 2 }
+    validates :password, length: { in: 6..20 }
+    validates :email, presence: true, uniqueness: true#, message: "that email has already been used"
 
     def name 
         self.first_name + " " + self.last_name
