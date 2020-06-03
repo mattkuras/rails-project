d2703c306920df3fdbe07f89848a6a1f545ee4fb
matt_kuras <mattkuras@gmail.com>
2020-06-03 18:07:15 -0400

    add community id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_192611) do
+ActiveRecord::Schema.define(version: 2020_06_03_214501) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_192611) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "community_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
