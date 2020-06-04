f5216a039f88fc811bbc6e572c94b0b1e218f35e
matt_kuras <mattkuras@gmail.com>
2020-06-04 13:45:57 -0400

    add broken scope method to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,15 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes 
   
+  scope :community_posts, -> {where(community_id: params[:community_id])}
+  #Community.find_by(id: params[:community_id]).posts
+
   def last_updated 
     updated_at.strftime("From %A, %b %d")
   end
+end
+
+class Article < ApplicationRecord
+  scope :published,               -> { where(published: true) }
+  scope :published_and_commented, -> { published.where("comments_count > 0") }
 end
